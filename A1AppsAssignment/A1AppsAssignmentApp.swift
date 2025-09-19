2ad99d8179941ed5fbf2dde408913265f29c92d3
dramaticking99 <cksanwariya6@gmail.com>
2025-09-19 22:22:51 +0530

    Initial Commit

---
+++ A1AppsAssignment/A1AppsAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  A1AppsAssignmentApp.swift
+//  A1AppsAssignment
+//
+//  Created by Chetan Sanwariya on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct A1AppsAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
